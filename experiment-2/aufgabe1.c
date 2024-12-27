710080993e8e0659ee66e9640663983c2eabb16b
Michał Roziel <rozielmichal@gmail.com>
2024-12-27 21:05:21 +0100

    organized experiment 1 and started experiment 2

---
+++ experiment-2/aufgabe1.c
@@ -0,0 +1,37 @@
+/********************************************************************/
+/*  Hochschule fuer Technik und Wirtschaft                          */
+/*  Fakultät fuer Ingenieurwissenschaften                           */
+/*  Labor fuer Eingebettete Systeme                                 */
+/*  Mikroprozessortechnik                                           */
+/********************************************************************/
+/*                                                                  */
+/*  C_Übung.C:                                                      */
+/*    Programmrumpf fuer C-Programme mit dem Keil                   */
+/*    Entwicklungsprogramm uVision fuer ARM-Mikrocontroller         */
+/*                                                                  */
+/********************************************************************/
+/*  Aufgaben-Nr.:        *                                          */
+/*                       *                                          */
+/*********************************                                  */
+/*                       *                                          */
+/********************************************************************/
+/*  Name / Matrikel-Nr.: *      Valentin Straßer                    */
+/*                       *      Michal Roziel                       */
+/*                       *                                          */
+/********************************************************************/
+/*  Abgabedatum:         *                                    		  */
+/*                       *                                          */
+/********************************************************************/
+
+#include <LPC21xx.H>        /* LPC21xx Definitionen                 */
+
+int main(void) {
+    unsigned char bcd;
+    
+    
+    while (1) {
+       
+    }
+}
+
+
