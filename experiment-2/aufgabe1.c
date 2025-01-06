82bc420b91a64bb16529de3c4bd40313a8fee1a8
Michał Roziel <rozielmichal@gmail.com>
2025-01-06 17:53:39 +0100

    started experiment 2

--- experiment-2/aufgabe1.c
+++ experiment-2/aufgabe1.c
@@ -12,26 +12,38 @@
 /********************************************************************/
 /*  Aufgaben-Nr.:        *                                          */
 /*                       *                                          */
-/*********************************                                  */
+/********************************************************************/
 /*                       *                                          */
 /********************************************************************/
 /*  Name / Matrikel-Nr.: *      Valentin Straßer                    */
 /*                       *      Michal Roziel                       */
 /*                       *                                          */
 /********************************************************************/
-/*  Abgabedatum:         *                                    		  */
+/*  Abgabedatum:         *      16.01.2025                          */
 /*                       *                                          */
 /********************************************************************/
 
 #include <LPC21xx.H>        /* LPC21xx Definitionen                 */
 
-int main(void) {
-    unsigned char bcd;
-    
-    
-    while (1) {
-       
-    }
+#define SEGMENT 0x1FC0000
+
+unsigned long bcd[10]={0x0FC0000, 0x180000, 0x16C0000, 0x13C0000, 0x1980000,
+ 0x1B40000, 0x1F40000, 0x1C0000, 0x1FC0000, 0x1BC0000};
+
+int main(void){
+									
+	IODIR0=SEGMENT;  		       	// Ausgang von P0.18-P.0.24 (BCD Anzeige) - SETZT 
+	IOCLR0=SEGMENT;	       	        	 // Setzt Low-Spannungspegel an P0.18-P0.24
+	IOSET0=bcd[0];				// Initialisiert den BCD auf 0 	
+
+	 		
+     while(1){
+
+	int eingabe=(IOPIN0>>10)&0xf;		// Nach rechts schieben 
+	IOCLR0=SEGMENT;				// Setzt Low-Spannungspegel an P0.18-P0.24
+	IOSET0=bcd[eingabe];			// Setzt Highspannungspegel am BCD 
+     }
 }
+
 
 
