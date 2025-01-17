893f6889b427269b8db837650d3f589a11ae9c8d
Michał Roziel <rozielmichal@gmail.com>
2025-01-17 16:23:38 +0100

    added new experiment 3 dir

---
+++ experiment-3-new/C_Uebung/C_Uebung.c
@@ -0,0 +1,38 @@
+/********************************************************************/
+/*  Hochschule fuer Technik und Wirtschaft                          */
+/*  Fakult�t fuer Ingenieurwissenschaften                           */
+/*  Labor fuer Eingebettete Systeme                                 */
+/*  Mikroprozessortechnik                                           */
+/********************************************************************/
+/*                                                                  */
+/*  C_�bung.C:                                                      */
+/*	  Programmrumpf fuer C-Programme mit dem Keil                   */
+/*    Entwicklungsprogramm uVision fuer ARM-Mikrocontroller         */
+/*                                                                  */
+/********************************************************************/
+/*  Aufgaben-Nr.:        *          Versuch 3                       */
+/*                       *                                          */
+/********************************************************************/
+/*  Gruppen-Nr.: 	     *                                          */
+/*                       *                                          */
+/********************************************************************/
+/*  Name / Matrikel-Nr.: *        Valentin Straßer    5014379       */
+/*                       *        Michal Roziel       5012845       */
+/*                       *                                          */
+/********************************************************************/
+/* 	Abgabedatum:         *        23.01.2025                        */
+/*                       *                                          */
+/********************************************************************/
+
+#include <LPC21xx.H>		/* LPC21xx Definitionen                 */
+
+int main (void)
+{
+	/* Initialisierung */
+
+ 	/* Endlosschleife */
+ 	while (1)
+	{
+		;
+	}
+}
