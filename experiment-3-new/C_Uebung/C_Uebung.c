1fb81ea2d2d1173faccc108787c334f0545cb9b0
Michał Roziel <rozielmichal@gmail.com>
2025-01-19 12:48:07 +0100

    added new code

--- experiment-3-new/C_Uebung/C_Uebung.c
+++ experiment-3-new/C_Uebung/C_Uebung.c
@@ -25,14 +25,43 @@
 /********************************************************************/
 
 #include <LPC21xx.H>		/* LPC21xx Definitionen                 */
+#include "C_Uebung.H"
 
-int main (void)
-{
-	/* Initialisierung */
+#define	PCLOCK 12500
+#define	BAUDRATE	4800
 
- 	/* Endlosschleife */
- 	while (1)
-	{
-		;
-	}
+/*
+int main(void) {
+  unsigned int frequenzteiler = PCLOCK / (16 * BAUDRATE);
+    while (1) {
+
+    }
+}
+*/
+int main(void){
+  unsigned int data;
+  /* Port-Pins konfigurieren */
+    PINSEL0 = PINSEL0 | 0x50000;/* P0.8=TxD, P0.9=RxD UART1 */
+  /* serielle Schnittstelle konfigurieren */
+    U1LCR = 0x83; /* 8-Bit pro Datenbyte, 1 Stop-Bit,
+  keine Parität, DLAB-Bit setzen */
+
+  /* Divisor-Berechnung für 9600 Baud bei p-clock = 30Mhz
+  30*10^6 / (9600*16) = 195 */
+    U1DLL = 195; /* Baudrate setzen */
+    U1DLM = 0;
+    U1LCR = 0x03; /* DLAB-Bit loeschen */
+    U1FCR = 0x07; /* FIFO's aktivieren und ruecksetzen */
+
+/* Hauptschleife */
+while (1) {
+  /* test auf Datenempfang */
+    if ((U1LSR & 0x01) != 0) {
+      data = U1RBR; /* empfangene Daten auslesen */
+    /* versenden der Daten */
+      if ((U1LSR & 0x20) != 0) { /* Senderegister frei? */
+      U1THR = data; /* eintragen in sende-FIFO */
+      }
+    }
+  }
 }
