99884ab1d4ab3cf53537f9ecd7b6e57f93eb7b30
Valentin S. <akirpap03@gmail.com>
2025-01-19 22:37:41 +0100

    adding some stuff to 3 and 4

--- experiment-4/C_Uebung/C_Uebung.c
+++ experiment-4/C_Uebung/C_Uebung.c
@@ -27,21 +27,50 @@
 #include <LPC21xx.H>		/* LPC21xx Definitionen                 */
 #include "C_Uebung.H"
 
-#define	PCLOCK 12.5
-#define	BAUDRATE	4800
+#define			PCLOCK 12.5
+#define 		SWITCH_MASK12      0x30000                // Schalter an P0.16 und P0.17
+#define			SWITCH_MASK3				0x1000000
+#define 		INPUT_MASK       0x3C00     		        // Eingabemaske für BCD
+static const unsigned int baudrates[] = {
+    110,     // 0
+    300,     // 1
+    600,     // 2
+    1200,    // 3
+    2400,    // 4
+    4800,    // 5
+    9600,    // 6
+    19200,   // 7
+    38400,   // 8
+    57600    // 9
+};
 
-
-/*
-int main(void) {
-  unsigned int frequenzteiler = PCLOCK / (16 * BAUDRATE);
-    while (1) {
-
-t}
-*/
 void initUart(void);
 
+unsigned initBaudrate(void);
+
+void initBCD(void);
+
+
+
+// Eingabewert von Schaltern einlesen
+unsigned int readInputBCD(void) {
+    return (IOPIN0 >> 10) & 0xF;  				        	// Bits 10-13 ausmaskieren
+}
+
+unsigned int initBaudrate(void){
+		unsigned int index = readInputBCD();
+		if(index > 9) index = 9;
+		return baudrates[index];
+	
+}
+
+// Schalter-Status einlesen
+unsigned int readSwitchState(void) {
+    return (IOPIN0 & SWITCH_MASK12) >> 16;  // Bits 16-17 ausmaskieren
+}
+
 void initUart(void) {
-    unsigned long Frequenzteiler = PCLOCK / (16 * BAUDRATE); // Calculate the frequency divider
+    unsigned long Frequenzteiler = PCLOCK / (16 * initBaudrate()); // Calculate the frequency divider
     U0DLL = Frequenzteiler % 256; // Low-byte (remainder of division by 256)
     U0DLM = Frequenzteiler / 256; // High-byte (integer division by 256)
 	  U0LCR = 0x03; /* DLAB-Bit loeschen */
@@ -53,6 +82,7 @@ int main(void){
   /* Port-Pins konfigurieren */
     PINSEL0 = PINSEL0 |0x05; /* 		UART0 Initialisierung 		*/
 		initUart();
+	  initBaudrate();
 
 /* Hauptschleife */
 while (1) {
