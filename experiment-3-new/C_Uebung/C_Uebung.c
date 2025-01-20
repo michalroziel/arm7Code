1b416a639b5ecd95175a619f903eb28a700f7c6b
Valentin S. <akirpap03@gmail.com>
2025-01-20 18:08:21 +0100

    e

--- experiment-3-new/C_Uebung/C_Uebung.c
+++ experiment-3-new/C_Uebung/C_Uebung.c
@@ -38,36 +38,49 @@ void sendchar (unsigned char daten);
 
 void initUart(unsigned int BaudRate, unsigned int DatenBits,unsigned int StoppBits,unsigned int ParitätAuswahl,unsigned int ParitätAktivierung) {
 	  unsigned int komparameter = ((((((0b1000 +ParitätAuswahl)<<1)+ParitätAktivierung)<<1)+StoppBits)<<2)+DatenBits;
-	  PINSEL0 = PINSEL0 |0x05; /* 		UART0 Initialisierung 		*/
+	  PINSEL0 = PINSEL0 | 0x50000;/* P0.8=TxD, P0.9=RxD UART1 */
     unsigned int Frequenzteiler = PCLOCK / (16 * BaudRate); // Calculate the frequency divider
-		U0LCR = komparameter;
-    U0DLL = Frequenzteiler % 256; // Low-byte (remainder of division by 256)
-    U0DLM = Frequenzteiler / 256; // High-byte (integer division by 256)
-	  U0LCR = 0x03; /* DLAB-Bit loeschen */
-    U0FCR = 0x07; /* FIFO's aktivieren und ruecksetzen */
+		U1LCR = komparameter;
+    U1DLL = Frequenzteiler % 256; // Low-byte (remainder of division by 256)
+    U1DLM = Frequenzteiler / 256; // High-byte (integer division by 256)
+	  U1LCR = 0x03; /* DLAB-Bit loeschen */
+    U1FCR = 0x07; /* FIFO's aktivieren und ruecksetzen */
 }
 
 void sendchar (unsigned char daten){
-	while ((U0LSR & 0x20) == 0);
-	if ( (U0LSR & 0x20) != 0 ) {
-	   		U0THR = daten;
+	while ((U1LSR & 0x20) == 0);
+	if ( (U1LSR & 0x20) != 0 ) {
+	   		U1THR = daten;
 	   }
 }
+
+void sendchars(char* daten) {
+    int j = 0;
+
+    while (daten[j] != '\0') {  // Läuft durch den String, bis das Nullterminierungszeichen erreicht wird
+        while ((U1LSR & 0x20) == 0);  // Wartet, bis das U0THR bereit ist
+        if ((U1LSR & 0x20) != 0) {
+            U1THR = daten[j];  // Überträgt das aktuelle Zeichen
+        }
+        j++;  // Gehe zum nächsten Zeichen
+    }
+}
+
 
 int main(void){
   unsigned int data;
   /* Port-Pins konfigurieren */
    
 		initUart();
-
+	  sendchars();
 /* Hauptschleife */
 while (1) {
   /* test auf Datenempfang */
-    if ((U0LSR & 0x01) != 0) {
+    if ((U1LSR & 0x01) != 0) {
       data = U0RBR; /* empfangene Daten auslesen */
     /* versenden der Daten */
-      if ((U0LSR & 0x20) != 0) { /* Senderegister frei? */
-      U0THR = data; /* eintragen in sende-FIFO */
+      if ((U1LSR & 0x20) != 0) { /* Senderegister frei? */
+      U1THR = data; /* eintragen in sende-FIFO */
       }
     }
   }
