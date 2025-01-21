0fef8bf3091940656bed9003c27325695117ad66
Valentin S. <60316714+huwano@users.noreply.github.com>
2025-01-21 17:17:43 +0100

    working version

--- experiment-3-new/C_Uebung/C_Uebung.c
+++ experiment-3-new/C_Uebung/C_Uebung.c
@@ -1,35 +1,41 @@
-#include <LPC21xx.H>
+/********************************************************************/
+/*  Hochschule fuer Technik und Wirtschaft                          */
+/*  Fakultät fuer Ingenieurwissenschaften                           */
+/*  Labor fuer Eingebettete Systeme                                 */
+/*  Mikroprozessortechnik                                           */
+/********************************************************************/
+/*                                                                  */
+/*  C_Übung.C:                                                      */
+/*    Programmrumpf fuer C-Programme mit dem Keil                   */
+/*    Entwicklungsprogramm uVision fuer ARM-Mikrocontroller         */
+/*                                                                  */
+/********************************************************************/
+/*  Aufgaben-Nr.:        *                                          */
+/*                       *                                          */
+/********************************************************************/
+/*                       *                                          */
+/********************************************************************/
+/*  Name / Matrikel-Nr.: *      Valentin Straßer                    */
+/*                       *      Michal Roziel                       */
+/*                       *                                          */
+/********************************************************************/
+/*  Abgabedatum:         *      16.01.2025                          */
+/*                       *                                          */
+/********************************************************************/
 
-#define PCLOCK 12500000 // 12.6 MHz
-#define BAUDRATE 19200  
+#include <LPC21xx.H>        /* LPC21xx Definitionen                 */
+
+#define PCLOCK 12500000
+#define BAUDRATE 19200
 
 void initUart(unsigned int BaudRate, unsigned int DatenBits, unsigned int StoppBits, unsigned int ParitaetAuswahl, unsigned int ParitaetAktivierung);
-void sendchar(unsigned char daten);
+void sendchar(char daten);
 void sendchars(char* daten);
-void readHexInput(char* buffer);
-void sendHexDump(unsigned int address);
-void intToStr(int value, char* str);
-int strLength(const char* str);
-unsigned long hexToDecimal(const char* hex);
+unsigned long readHexInput(void);
+char readChar(void);
+void sendlong(unsigned long number);
+void sendHexFromMemory(unsigned long address, int length) ;
 
-unsigned long hexToDecimal(const char* hex) {
-    unsigned long result = 0;
-    char c;
-    while ((c = *hex++) != '\0') {
-        result <<= 4; // Multiplikation mit 16 (Shift um 4 Bits nach links)
-        if (c >= '0' && c <= '9') {
-            result += c - '0';
-        } else if (c >= 'A' && c <= 'F') {
-            result += c - 'A' + 10;
-        } else if (c >= 'a' && c <= 'f') {
-            result += c - 'a' + 10;
-        } else {
-            // Ungültiges Zeichen ignorieren oder Fehlerbehandlung
-            break;
-        }
-    }
-    return result;
-}
 
 
 void initUart(unsigned int BaudRate, unsigned int DatenBits, unsigned int StoppBits, unsigned int ParitaetAuswahl, unsigned int ParitaetAktivierung) {
@@ -43,45 +49,29 @@ void initUart(unsigned int BaudRate, unsigned int DatenBits, unsigned int StoppB
 
     PINSEL0 |= 0x50000; // P0.8=TxD, P0.9=RxD UART1
     Frequenzteiler = PCLOCK / (16 * BaudRate);
-    U1LCR = komparameter;
+    U1LCR = 0x9F;
     U1DLL = Frequenzteiler % 256;
     U1DLM = Frequenzteiler / 256;
-    U1LCR = 0x03;
+    U1LCR = 0x1F;
     U1FCR = 0x07;
 }
 
-
-
-void sendchar(unsigned char daten) {
+void sendchar(char daten) {
     while ((U1LSR & 0x20) == 0);
     U1THR = daten;
 }
 
-void sendHexFromMemory(unsigned long address, int length) {
-    sendchars("Hex-Dump:\r\n");
-
-    // Zeiger auf die Adresse casten
-    unsigned char* ptr = (unsigned char*)address;
-
-    for (int i = 0; i < length; i++) {
-        unsigned char value = ptr[i];  // Lese ein Byte aus dem Speicher
-
-        // Oberes und unteres Nibble des Byte-Wertes in Hexadezimal umwandeln
-        char highNibble = (value >> 4) & 0x0F;
-        char lowNibble = value & 0x0F;
-
-        // Nibble in Hex-Zeichen umwandeln und senden
-        sendchar(highNibble < 10 ? '0' + highNibble : 'A' + (highNibble - 10));
-        sendchar(lowNibble < 10 ? '0' + lowNibble : 'A' + (lowNibble - 10));
-
-        sendchars(" ");  // Leerzeichen für bessere Lesbarkeit
+void sendchars(char* daten) {
+    int j = 0;
+    while (daten[j] != '\0') {
+        sendchar(daten[j]);
+        j++;
     }
-
-    sendchars("\r\n");  // Neue Zeile am Ende
+	}
+char readChar(void){
+	while (!(U1LSR & 0x01));
+        return U1RBR;
 }
-
-
-
 
 void sendlong(unsigned long number) {
     char buffer[20];  // Platz für maximal 20 Ziffern (ein long kann bis zu 19 Stellen haben)
@@ -97,32 +87,38 @@ void sendlong(unsigned long number) {
     while (index > 0) {
         sendchar(buffer[--index]);  // Puffer rückwärts durchlaufen und Zeichen senden
     }
-}
+	}
 
-void sendchars(char* daten) {
-    int j = 0;
-    while (daten[j] != '\0') {
-        sendchar(daten[j]);
-        j++;
+void sendHexFromMemory(unsigned long address, int length) {
+    // Zeiger auf die Adresse casten
+    char* ptr = (char*) address;
+		int i;
+    for (i = 0; i < length; i++) {
+        unsigned char value = ptr[i];  // Lese ein Byte aus dem Speicher
+
+        // Oberes und unteres Nibble des Byte-Wertes in Hexadezimal umwandeln
+        char highNibble = (value >> 4) & 0x0F;
+        char lowNibble = value & 0x0F;
+
+        // Nibble in Hex-Zeichen umwandeln und senden
+        sendchar(highNibble < 10 ? '0' + highNibble : 'A' + (highNibble - 10));
+        sendchar(lowNibble < 10 ? '0' + lowNibble : 'A' + (lowNibble - 10));
+
+        sendchars(" ");  // Leerzeichen für bessere Lesbarkeit
     }
 }
 
-unsigned long  readHexInput(void) {
-    int index = 0;
+unsigned long readHexInput(void) {
     char receivedChar;
-
-    unsigned long address = 0 ;
-
-    sendchars("readHexInput wurde aufgerufen");
-
-while (index < 8) {
-        receivedChar = readChar();  // Verwende readChar() zum Empfang eines Zeichens
-
-        if (receivedChar == '\r') {
+		unsigned long address = 0;
+		int i;
+	  for (i = 0; i < 8; i++) {
+        receivedChar = readChar();
+			  sendchar(receivedChar);
+				if (receivedChar == '\r') {
             break;
         }
-
-        if (receivedChar >= '0' && receivedChar <= '9') {
+				if (receivedChar >= '0' && receivedChar <= '9') {
             address = (address << 4) + (receivedChar - '0');
         } 
         else if (receivedChar >= 'A' && receivedChar <= 'F') {
@@ -130,49 +126,28 @@ while (index < 8) {
         } 
         else if (receivedChar >= 'a' && receivedChar <= 'f') {
             address = (address << 4) + (receivedChar - 'a' + 10);
-        } 
-        else {
-            sendchars("Ungültiges Zeichen!\r\n");
-            continue;  // Ungültige Zeichen überspringen
+        } else {
+            sendchars("Ungueltiges Zeichen!\r\n");
+            i--;
         }
-
-        index++;
-        sendchar(receivedChar);  // Echo des eingegebenen Zeichens
     }
-
-      return address;
+		return address;
 }
 
 
-char readChar(void) {
-    while (!(U1LSR & 0x01));  // Wait until data is available in the receiver buffer
-    return U1RBR;             // Read and return the received character
-}
-
-
-
-void sendHexDump(unsigned int address) {
-    char buffer[16];
-    char* ptr = (char*) address;
-
-    int i;
-    for (i = 0; i < 16; i++) {
-        buffer[i] = ptr[i];
-    }
-
-    sendchars(buffer);
-    sendchars("\r\n\r\n");
-}
 
 int main(void) {
-    char inputBuffer[9];
-    unsigned long address;
-
+    unsigned long address = 0;
     initUart(BAUDRATE, 3, 1, 1, 1);
-
     while (1) {
-        readHexInput(inputBuffer);
-        address = hexToDecimal(inputBuffer);
-        sendHexDump(address);
-    }
+			address = readHexInput();
+			sendchar('\n');
+			sendchars("Deine Addresse lautet: ");
+			sendlong(address);
+			sendchar('\n');
+			sendchars("\r\n");
+			sendchars("Addresse: ");
+			sendHexFromMemory(address,16);
+			sendchars("\r\n\n");  // Neue Zeile am Ende
+    }	
 }
