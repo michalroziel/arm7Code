aacc3cf8094330b4527673295047a0219ef083e9
Michał Roziel <rozielmichal@gmail.com>
2025-01-23 01:28:24 +0100

    commented C_Uebung.c

--- experiment-3-new/C_Uebung/C_Uebung.c
+++ experiment-3-new/C_Uebung/C_Uebung.c
@@ -29,70 +29,73 @@ void uartInit(unsigned int baudRate, unsigned int dataBits, unsigned int stopBit
     PINSEL0 |= UART_PINSEL_CONFIG;  // P0.8 = TxD1, P0.9 = RxD1 für UART1 aktivieren
 
     Frequenzteiler = PCLOCK / (16 * baudRate);  // Baudratenteiler berechnen
-    U1LCR = DLAB_BIT | uartConfig;  // DLAB-Bit setzen, 8 Datenbits, 1 Stoppbit
-    U1DLL = Frequenzteiler % 256;  // Niedriges Byte des Divisors
-    U1DLM = Frequenzteiler / 256;   // Hohes Byte des Divisors
-    U1LCR = uartConfig;  // DLAB-Bit löschen
-    U1FCR = UART_FIFO_ENABLE;  // FIFO aktivieren und zurücksetzen
+                                                // DLAB : einstellung BAUD
+    U1LCR = DLAB_BIT | uartConfig;  						// DLAB-Bit setzen, 8 Datenbits, 1 Stoppbit
+    U1DLL = Frequenzteiler % 256;  							// Niedriges Byte des FrequenzTeilers
+    U1DLM = Frequenzteiler / 256;   						// Hohes Byte des FrequenzTeilers
+    U1LCR = uartConfig;													// DLAB-Bit löschen
+    U1FCR = UART_FIFO_ENABLE;  									// FIFO aktivieren und zurücksetzen
 }
 
 // Zeichen über UART senden
 void uartSendChar(char data) {
-    while ((U1LSR & UART_READY_BIT) == 0);  // Warte, bis das Transmit-Register bereit ist
-    U1THR = data;  // Zeichen senden
+    while ((U1LSR & UART_READY_BIT) == 0);  		// Warte, bis das Transmit-Register bereit ist
+    U1THR = data;  															// char send, sobald beschrieben -> senden
 }
 
 // String über UART senden
 void uartSendString(char* str) {
     int i = 0;
-    while (str[i] != '\0') {
-        uartSendChar(str[i]);  // Zeichenweise senden
+    while (str[i] != '\0') {										// Null Terminierung
+        uartSendChar(str[i]); 								  // Zeichenweise senden
         i++;
     }
 	}
 
 // Zeichen über UART empfangen
 char uartReadChar(void) {
-    while (!(U1LSR & UART_RX_READY));  // Warte auf empfangenes Zeichen
-    return U1RBR;  // Zeichen zurückgeben
-}
+    while (!(U1LSR & UART_RX_READY));  					// Warte auf empfangenes Zeichen
+    return U1RBR;  															// Zeichen zurückgeben
+}																								// DR Bit - Bit 0 in LSR
 
+// Hex Zeichen in numerischen Wert umwandeln
 unsigned int hexCharToValue(char c) {
     if (c >= '0' && c <= '9') {
-        return c - '0';
-    } else if (c >= 'A' && c <= 'F') {
-        return c - 'A' + 10;
+        return c - '0';													// '0' = 48 , '9' = 57
+    } else if (c >= 'A' && c <= 'F') {					// 'A' = 65 , 'F' = 70
+        return c - 'A' + 10;										// 'c' = 97 , 'f' = 102
     } else if (c >= 'a' && c <= 'f') {
         return c - 'a' + 10;
     }
-    return INVALID_HEX_VALUE;  // Ungültiges Zeichen
+    return INVALID_HEX_VALUE;  									// Ungültiges Zeichen
 }
 
 // Hexadezimale Adresse von Benutzer lesen
 void uartReadHexInput(char* inputBuffer, unsigned long* address) {
     char receivedChar;
 	  unsigned int hexValue;
-    int i=0;
-    *address = 0;  // Adresse initialisieren
+    int i=0;																		// Anzahl der empf. zeichen
+    *address = 0;  															// Adresse init -> um aufzubauen
 
     while (i < HEX_DIGIT_LIMIT) {
-        receivedChar = uartReadChar();  // Zeichen empfangen
-        uartSendChar(receivedChar);  // Echo des Zeichens
+        receivedChar = uartReadChar();  				// Zeichen empfangen
+        uartSendChar(receivedChar);  						// Echo des Zeichens -> bestätigung
 
-        if (receivedChar == '\r') {  // Eingabe beenden
+        if (receivedChar == '\r') { 					  // Eingabe beenden
             break;
         }
-				// Umwandlung des Zeichens in einen Hex-Wert
+																								// Umwandlung des Zeichens in Hex-Wert
         hexValue = hexCharToValue(receivedChar);
         if (hexValue != INVALID_HEX_VALUE) {
-							// Adresse aktualisieren und Zeichen in den Puffer schreiben
+																								// ADDR UPDATE & Zeichen in Puffer schreiben
 						*address = (*address << 4) + hexValue;
 						inputBuffer[i++] = receivedChar;
 						} else {
 					   uartSendString(" Ungültiges Zeichen! Bitte nur Hex-Zeichen verwenden (0-9, a-f, A-F)." CR_LF);
 				    }
     }
-				inputBuffer[i] = '\0';  // Nullterminierung für String
+				inputBuffer[i] = '\0';  								// Nullterminierung für String
+																								// Zusätzliche Zeichen nach Eingabe verwerfen
 		while (receivedChar != '\r') {
         receivedChar = uartReadChar();
         uartSendChar(receivedChar);
@@ -103,30 +106,24 @@ void uartReadHexInput(char* inputBuffer, unsigned long* address) {
 
 }
 
-// Speicherinhalt als Hexadezimalwerte ausgeben
+// Speicherinhalt als Hexadezimalwerte ausgeben - Jedes Byte als Zwei Zeichen ausgeben
 void memoryDumpHex(unsigned long address, unsigned int length) {
-    char* ptr = (char*) address;  // Speicheradresse auf char Zeiger umwandeln,
-    int i;                        // um Byteweise zugreifen zu können
+    char* ptr = (char*) address;  							// Speicheradresse auf char Zeiger umwandeln,
+    int i;                       							  // um Byteweise zugreifen zu können
     for (i = 0; i < length; i++) {
-        unsigned char value = ptr[i];  // Jedes Byte nacheinander auslesen
+        unsigned char value = ptr[i];           // aktuelles Byte auslesen
 
-        // Upper-ByteHälfte und Lower-ByteHälfte extrahieren
-        // Wir shiften um 4 und maskieren die restlichen Bits
-        char upperHalf = (value >> 4) & 0x0F;
-        char lowerHalf = value & 0x0F;
+				// Upper-Byte Hälfte und Lower-ByteHälfte extrahieren
+        char upperHalf = (value >> 4) & 0x0F    // um 4 shiften und mask. zum extrahieren
+        char lowerHalf = value & 0x0F;					// Bit-maske
 
-        // numerischen Wert als ASCII anzeigen:
-        // '0' hat den Wert 48, 'A' hat den Wert 65.
-        // Wenn upperHalf < 10 ist, wird '0' + upperHalf ausgeführt.
-        // Bsp :  48 + 5  = 53  (-> 5 in ASCII)
-        // Bsp :  65 + (B - 10) (-> 66 = B in ASCII)
         uartSendChar(upperHalf < 10 ? '0' + upperHalf : 'A' + (upperHalf - 10));
-        // lowerHalf analog zu upperHalf
+																								// lowerHalf analog zu upperHalf
         uartSendChar(lowerHalf < 10 ? '0' + lowerHalf : 'A' + (lowerHalf - 10));
 
-        uartSendChar(' ');  // Leerzeichen für visuelles
+        uartSendChar(' ');  										// Leerzeichen für visuelles
     }
-    // Sende eines Returns und Line-Feed
+																								// Sende eines Returns und Line-Feed
     uartSendString(CR_LF);
 }
 
